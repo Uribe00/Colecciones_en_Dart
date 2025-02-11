325d6da3e784305ca061b55cb723a827b528d7b6
Daniel Uribe <a.22308051281323@cbtis128.edu.mx>
2025-02-11 16:53:54 +0000

    hola lista

---
+++ lista1.dart
@@ -0,0 +1,12 @@
+void main() {  
+   List<int> numeros = [10, 20, 30];  
+   print("Lista de números entero:");
+   print(numeros);
+   print ("Primer elemento: ${numeros[0]}");
+   print ("Segundo elemento: ${numeros[1]}");  
+   print ("Tercer elemento: ${numeros[2]}");
+   print("Elementos de la lista usando ciclo for:");
+   for (int i = 0; i < numeros.length; i++) {  
+      print(numeros[i]);  
+   }
+}  
